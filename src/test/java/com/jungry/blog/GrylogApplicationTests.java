91d7f3226c5fdedd5c43e92d9a18715d61090f78
jungry <seojungrain@gmail.com>
2022-02-24 13:05:44 +0900

    preference

---
+++ src/test/java/com/jungry/blog/GrylogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jungry.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrylogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
